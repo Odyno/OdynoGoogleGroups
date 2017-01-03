69b67238928089cbee2ecd1fb6991dcdc2ff88fe
Alessandro Staniscia <astaniscia@navionics.com>
2017-01-03 19:52:50 +0100

    cosmetic fix

--- odyno-google-groups-lib.php
+++ odyno-google-groups-lib.php
@@ -33,17 +33,17 @@
  *
  */
 function do_odyno_google_groups(
-	$name, $id = null, $width = "100%", $height = "800px", $showsearch = "false",
-	$showtabs = "false", $hideforumtitle = "true", $hidesubject = "true", $domain = null
+  $name, $id = NULL, $width = "100%", $height = "800px", $showsearch = "false",
+  $showtabs = "false", $hideforumtitle = "true", $hidesubject = "true", $domain = NULL
 ) {
 
-	if ( $id == null ) {
-		$id = uniqid( "", true );
-	}
+  if ($id == NULL) {
+    $id = uniqid("", TRUE);
+  }
 
-	do_action( "pre_ogg_show", $name, $id, $width, $height, $showsearch, $showtabs, $hideforumtitle, $hidesubject, $domain );
-	echo odyno_google_groups_get_page( $name, $id, $width, $height, $showsearch, $showtabs, $hideforumtitle, $hidesubject, $domain );
-	do_action( "post_ogg_show", $name, $id, $width, $height, $showsearch, $showtabs, $hideforumtitle, $hidesubject, $domain );
+  do_action("pre_ogg_show", $name, $id, $width, $height, $showsearch, $showtabs, $hideforumtitle, $hidesubject, $domain);
+  echo odyno_google_groups_get_page($name, $id, $width, $height, $showsearch, $showtabs, $hideforumtitle, $hidesubject, $domain);
+  do_action("post_ogg_show", $name, $id, $width, $height, $showsearch, $showtabs, $hideforumtitle, $hidesubject, $domain);
 }
 
 
@@ -55,30 +55,30 @@ function do_odyno_google_groups(
  * @return string
  */
 function odyno_google_groups_get_page(
-	$name, $id = null, $width = "100%", $height = "800px", $showsearch = "false",
-	$showtabs = "false", $hideforumtitle = "true", $hidesubject = "true", $domain = null
+  $name, $id = NULL, $width = "100%", $height = "800px", $showsearch = "false",
+  $showtabs = "false", $hideforumtitle = "true", $hidesubject = "true", $domain = NULL
 ) {
 
-	if ( $id == null ) {
-		$id = uniqid( "", true );
-	}
+  if ($id == NULL) {
+    $id = uniqid("", TRUE);
+  }
 
-	if ( $domain == null ) {
-		$domain = ODY_GOOGLE_GROUPS_NOS;
-	}
+  if ($domain == NULL) {
+    $domain = ODY_GOOGLE_GROUPS_NOS;
+  }
 
-	$isSigned  = get_option( ODY_GG_SHOW_SIGNE, true );
-	$isContrib = get_option( ODY_GG_ENABLED_ANALITYC, true );
+  $isSigned  = get_option(ODY_GG_SHOW_SIGNE, TRUE);
+  $isContrib = get_option(ODY_GG_ENABLED_ANALITYC, TRUE);
 
-	if ( $name == ODY_GOOGLE_GROUPS_NOS ) {
-		$out = 'Minimal information of tag: [google_groups name="name-of-group" ]';
-	} else {
-		$out = odyno_google_groups_page_shortcut_html( $isContrib, $isSigned, $id, $width, $height );
-		//add_action('wp_footer', 'odyno_google_groups_page_shortcut_js', $id, $name, $domain, $showsearch, $showtabs, $hideforumtitle, $hidesubject, $showsearch,8);
-		$out .= odyno_google_groups_page_shortcut_js( $id, $name, $domain, $showsearch, $showtabs, $hideforumtitle, $hidesubject, $showsearch );
-	}
+  if ($name == ODY_GOOGLE_GROUPS_NOS) {
+    $out = 'Minimal information of tag: [google_groups name="name-of-group" ]';
+  }
+  else {
+    $out = odyno_google_groups_page_shortcut_html($isContrib, $isSigned, $id, $width, $height);
+    $out .= odyno_google_groups_page_shortcut_js($id, $name, $domain, $showsearch, $showtabs, $hideforumtitle, $hidesubject, $showsearch);
+  }
 
-	return $out;
+  return $out;
 }
 
 /**
@@ -89,21 +89,22 @@ function odyno_google_groups_get_page(
  * @param type $width
  * @param type $height
  */
-function odyno_google_groups_page_shortcut_html( $isContrib, $isSigned, $id, $width, $height ) {
+function odyno_google_groups_page_shortcut_html($isContrib, $isSigned, $id, $width, $height) {
 
-	if ( $isSigned ) {
-		$sign_inline_style = "color: #000; font-family: arial,sans-serif; text-align: right; font-size: 77%;";
-	} else {
-		$sign_inline_style = "display: none;";
-	}
+  if ($isSigned) {
+    $sign_inline_style = "color: #000; font-family: arial,sans-serif; text-align: right; font-size: 77%;";
+  }
+  else {
+    $sign_inline_style = "display: none;";
+  }
 
-	$contribCode = "";
+  $contribCode = "";
 
-	if ( $isContrib ) {
-		$contribCode = '<img style=\'display: none;\' src="http://www.staniscia.net/ogg/logo.php?t=gif" alt="logo" onerror="this.parentNode.removeChild(this)" />';
-	}
+  if ($isContrib) {
+    $contribCode = '<img style=\'display: none;\' src="http://www.staniscia.net/ogg/logo.php?t=gif" alt="logo" onerror="this.parentNode.removeChild(this)" />';
+  }
 
-	$out = '
+  $out = '
 			<div id="' . $id . '_odynoggroups_div" class="odynoggroups_div" >
 				<iframe
 				     id="' . $id . '_ogg_forum_embed"
@@ -112,45 +113,58 @@ function odyno_google_groups_page_shortcut_html( $isContrib, $isSigned, $id, $wi
 					 scrolling="no"
 					 style="border: 0; width:' . $width . '; height:' . $height . ';" >
 				</iframe>
-				<div style=\'' . $sign_inline_style . '\'>' . __( 'powered by <a href="http://www.staniscia.net/OdynoGoogleGroups/">Odyno gGroups</a>', 'odynogooglegroups' ) . $contribCode . '</div>
+				<div style=\'' . $sign_inline_style . '\'>' . __('powered by <a href="http://www.staniscia.net/OdynoGoogleGroups/">Odyno gGroups</a>', 'odynogooglegroups') . $contribCode . '</div>
            </div>';
 
-	if ( WP_DEBUG ) {
-		$out = "\n\n\n<!-- " . OGG_START_DEBUG_TAG . " -->\n\n\n" . $out . "\n\n\n<!-- " . OGG_STOP_DEBUG_TAG . " -->\n\n\n";
-	}
+  if (WP_DEBUG) {
+    $out = "\n\n\n<!-- " . OGG_START_DEBUG_TAG . " -->\n\n\n" . $out . "\n\n\n<!-- " . OGG_STOP_DEBUG_TAG . " -->\n\n\n";
+  }
 
-	return $out;
+  return $out;
 }
 
+/**
+ * Get the JS
+ * @param $id
+ * @param $name
+ * @param $domain
+ * @param $showsearch
+ * @param $showtabs
+ * @param $hideforumtitle
+ * @param $hidesubject
+ * @param $showsearch
+ *
+ * @return string
+ */
 function odyno_google_groups_page_shortcut_js(
-	$id,
-	$name,
-	$domain,
-	$showsearch,
-	$showtabs,
-	$hideforumtitle,
-	$hidesubject,
-	$showsearch
+  $id,
+  $name,
+  $domain,
+  $showsearchParam,
+  $showtabs,
+  $hideforumtitle,
+  $hidesubject,
+  $showsearch
 ) {
 
-	$out = '<script type="text/javascript">
+  $out = '<script type="text/javascript">
              document.getElementById("' . $id . '_ogg_forum_embed").src ="https://groups.google.com/forum/embed/?place=forum/' . $name;
-	$out .= ( $domain == ODY_GOOGLE_GROUPS_NOS ) ? '' : '&domain=' . $domain;
-	$out .= "&theme=default";
-	$out .= "&fragments=true";
-	$out .= "&showsearch=" . $showsearch;
-	$out .= "&showtabs=" . $showtabs;
-	$out .= "&hideforumtitle=" . $hideforumtitle;
-	$out .= "&hidesubject=" . $hidesubject;
-	$out .= "&parenturl=\"+encodeURIComponent(window.location.href);";
-	//NO// $out .= "&contenturl=" . urlencode(get_permalink());
-	$out .= "\n</script>";
+  $out .= ($domain == ODY_GOOGLE_GROUPS_NOS) ? '' : '&domain=' . $domain;
+  $out .= "&theme=default";
+  $out .= "&fragments=true";
+  $out .= "&showsearch=" . $showsearchParam;
+  $out .= "&showtabs=" . $showtabs;
+  $out .= "&hideforumtitle=" . $hideforumtitle;
+  $out .= "&hidesubject=" . $hidesubject;
+  $out .= "&parenturl=\"+encodeURIComponent(window.location.href);";
+  //NO// $out .= "&contenturl=" . urlencode(get_permalink());
+  $out .= "\n</script>";
 
-	if ( WP_DEBUG ) {
-		$out = "\n\n\n<!-- " . OGG_START_DEBUG_TAG . " -->\n\n\n" . $out . "\n\n\n<!-- " . OGG_STOP_DEBUG_TAG . " -->\n\n\n";
-	}
+  if (WP_DEBUG) {
+    $out = "\n\n\n<!-- " . OGG_START_DEBUG_TAG . " -->\n\n\n" . $out . "\n\n\n<!-- " . OGG_STOP_DEBUG_TAG . " -->\n\n\n";
+  }
 
-	return $out;
+  return $out;
 }
 
 
